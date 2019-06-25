f2604c97e5b22ec1f626428675b764b23e91c773
IntegrativeMechanobiologyLaboratory <46034811+IntegrativeMechanobiologyLaboratory@users.noreply.github.com>
2019-06-24 23:53:41 -0500

    Add files via upload

--- motors/driving.py
+++ motors/driving.py
@@ -4,36 +4,48 @@ from guizero import App, PushButton, Slider, Text
 
 def forward(value):
 	explorerhat.motor.forward(int(value))
-
+	backwardSlider.value = "0"
+	leftSlider.value = "0"
+	rightSlider.value = "0"
+	
 def backward(value):
         explorerhat.motor.backward(int(value))
+        forwardSlider.value = "0"
+        leftSlider.value = "0"
+        rightSlider.value = "0"
 	
 def stop():	
 	explorerhat.motor.stop()
-	forwardSider.value = "0"
+	forwardSlider.value = "0"
 	backwardSlider.value = "0"
-	leftSider.value = "0"
+	leftSlider.value = "0"
 	rightSlider.value = "0"
 
 def turnLeft(value):
         explorerhat.motor.one.forward(int(value))
         explorerhat.motor.two.backward(int(value))
-        
+        forwardSlider.value = "0"
+        backwardSlider.value = "0"
+        rightSlider.value = "0"
+	
 def turnRight(value):
         explorerhat.motor.two.forward(int(value))
         explorerhat.motor.one.backward(int(value))
-
+        forwardSlider.value = "0"
+        backwardSlider.value = "0"
+        leftSlider.value = "0"
+	
 app = App("Controller", height=300, width=300)
 stopButton = PushButton(app, stop, text="Stop")
 
 forwardTxt = Text(app, "Move forward")
-forwardSider = Slider(app, start=0, end=100, command=forward)
+forwardSlider = Slider(app, start=0, end=100, command=forward)
 
 backwardTxt = Text(app, "Move backward")
 backwardSlider = Slider(app, start=0, end=100, command=backward)
 
 leftTxt = Text(app, "Turn left")
-leftSider = Slider(app, start=0, end=100, command=turnLeft)
+leftSlider = Slider(app, start=0, end=100, command=turnLeft)
 
 rightTxt = Text(app, "Turn right")
 rightSlider = Slider(app, start=0, end=100, command=turnRight)
